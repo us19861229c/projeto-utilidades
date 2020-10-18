ab35a26c69c9c14d2ade06476a3f83aa4e66487a
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-10-17 23:39:08 -0300

    acrescenta nomes de listas através do input, além de disparar uma alerta caso o usuário envie um nome vazio

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const containerLista = document.querySelector('[data-listas]');
+const novaListaFormulario = document.querySelector('[data-form-nova-lista]');
+const novaListaInput = document.querySelector('[data-input-nova-lista]');
+
 
 let listas = [
   {
@@ -12,15 +15,27 @@ let listas = [
     nome: 'Livros'
   }]; 
 
+novaListaFormulario.addEventListener('submit', e => {
+  e.preventDefault();
+  const nomeDaLista = novaListaInput.value;
+  if (nomeDaLista == null || nomeDaLista == '') return alert('Por favor,\ndigite um nome de lista');
+  const lista = criarLista(nomeDaLista);
+  novaListaInput.value = null;
+  listas.push(lista);
+  renderizar();
+})
+
+function criarLista(nome) {
+  return { id: Date.now().toString(), nome, tarefas: []}
+}
+  
 function renderizar() {
   limparElemento(containerLista);
   listas.forEach(lista => {
-    console.log(lista)
     const itemDeLista = document.createElement('li');
     itemDeLista.dataset.listaId = lista.id
     itemDeLista.innerText = lista.nome;
     containerLista.appendChild(itemDeLista);
-    console.log(itemDeLista)
   })
 }
 
