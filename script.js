61e5a4ecc92b63798c313a35f2e25b74c784e409
Rebeca Cerqueira <rcks.santos@gmail.com>
2020-10-17 23:11:11 -0300

    aprimora renderização da lista, lidando com objetos que possuem id e nome

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const containerLista = document.querySelector('[data-listas]');
 
-let listas = ['Projeto', 'Cookies', 'Livros']; 
+let listas = [
+  {
+    id: 1, 
+    nome: 'Projeto'
+  },{
+    id: 2, 
+    nome: 'Cookies'
+  },{
+    id: 3, 
+    nome: 'Livros'
+  }]; 
 
 function renderizar() {
   limparElemento(containerLista);
   listas.forEach(lista => {
     console.log(lista)
     const itemDeLista = document.createElement('li');
-    itemDeLista.innerText = lista;
+    itemDeLista.dataset.listaId = lista.id
+    itemDeLista.innerText = lista.nome;
     containerLista.appendChild(itemDeLista);
     console.log(itemDeLista)
   })
